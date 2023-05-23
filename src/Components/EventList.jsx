94f17721b75a7ed8ecd9dfc38b8e17300a22726e
wallace-y <cameronwfinney@gmail.com>
2023-05-23 08:21:42 +0100

    adding search functionatlity using useRef

--- src/Components/EventList.jsx
+++ src/Components/EventList.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import EventCard from "./EventCard";
 import EventSearch from "./EventSearch";
 
 function EventList() {
+  const [search, setSearch] = useState("London");
+
+  console.log(search);
+
   return (
     <div className="album py-5 bg-body-tertiary">
       <div className="container text-center">
         <h1>Events near you...</h1>
-        <EventSearch />
+        <EventSearch setSearch={setSearch} />
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
           <EventCard />
           <EventCard />
--- src/Components/EventSearch.jsx
+++ src/Components/EventSearch.jsx
@@ -1,18 +1,30 @@
-function EventSearch() {
+import { useRef } from "react";
+
+function EventSearch({ setSearch }) {
+  const searchRef = useRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSearch(searchRef.current.value);
+  }
+
   return (
     <form
+      onSubmit={handleSubmit}
       type="submit"
       className="row g-3 align-items-center justify-content-center mb-3"
     >
       <div className="col-auto">
-        <label for="search" class="form-label">
+        <label htmlFor="search" className="form-label">
           Search events here:
         </label>
       </div>
       <div className="col-auto">
         <input
-          class="form-control"
+          ref={searchRef}
+          className="form-control"
           id="search"
+          name="search"
           placeholder="Type to search..."
         ></input>
       </div>
